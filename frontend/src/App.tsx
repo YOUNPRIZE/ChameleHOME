9e923b82140ece705d7b1ce5084ad85a79a47566
Koneweekk <rlagkswn37@naver.com>
2023-07-20 17:37:09 +0900

    [Design] Jira : 142 1. Add Signup page 2. Change top bar 3. fix Login and Signup page content ratio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from "./pages/Main";
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,8 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import style from '../styles/Login.module.css';
+import { nowPageStore } from '../store/store';
 
 export default function Login():JSX.Element {
+  // 페이지명 변경
+  const changePage = nowPageStore(state => state.setPage);
+  changePage("로그인")
+
   // 유저 정보 input 값들
   const userId = useRef<HTMLInputElement>(null);
   const userPassword = useRef<HTMLInputElement>(null);
@@ -16,25 +21,23 @@ export default function Login():JSX.Element {
   }
 
   return (
-    <div>
-      <img src={reptile01} alt=""/>
-      <div className='w-75 mx-auto'>
-        <Form>
-          <Form.Group className="mb-3" controlId="inputId">
-            <Form.Label className='fw-bold fs-3'>아이디</Form.Label>
-            <Form.Control className="bg-secondary-subtle" type="text" placeholder="아이디를 입력해주세요" ref={userId}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="inputPassword">
-            <Form.Label className='fw-bold fs-3'>비밀번호</Form.Label>
-            <Form.Control className="bg-secondary-subtle" type="password" placeholder="비밀번호를 입력해주세요" ref={userPassword}/>
-          </Form.Group>
-          <div className='d-flex justify-content-between mt-3'>
-            <a href="#" className={style.additionalLink}>비밀번호 찾기</a>
-            <Link to="/SignUp" className={style.additionalLink}>회원가입</Link>
-          </div>
-          <Button size="lg" className={style.loginBtn} variant='success' onClick={Login}>로그인</Button>
-        </Form>
-      </div>
+    <div className={style.authForm}>
+      <Form className='w-75 mx-auto'>
+        <img src={reptile01} alt=""/>
+        <Form.Group className="mb-3" controlId="inputId">
+          <Form.Label className='fw-bold fs-3'>아이디</Form.Label>
+          <Form.Control className="bg-secondary-subtle" type="text" placeholder="아이디를 입력해주세요" ref={userId}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="inputPassword">
+          <Form.Label className='fw-bold fs-3'>비밀번호</Form.Label>
+          <Form.Control className="bg-secondary-subtle" type="password" placeholder="비밀번호를 입력해주세요" ref={userPassword}/>
+        </Form.Group>
+        <div className='d-flex justify-content-between mt-3'>
+          <a href="#" className={style.additionalLink}>비밀번호 찾기</a>
+          <Link to="/SignUp" className={style.additionalLink}>회원가입</Link>
+        </div>
+        <Button size="lg" className={style.loginBtn} variant='success' onClick={Login}>로그인</Button>
+      </Form>
     </div>
   )
 }
