1e6151622ba948f8c41cbc725b0518933d14a20c
Koneweekk <rlagkswn37@naver.com>
2023-07-21 16:47:09 +0900

    [Add, Feat] Jira: 152 1.Add icon on tob bar 2.Add back button feature

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
 import { nowPageStore } from '../store/store';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
@@ -7,24 +8,29 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Header():JSX.Element {
   // 현재 페이지명을 받아와서 표시
-  // const pageName =  nowPageStore(state => state.pageName);  // 현재 페이지 명
   const [pageName, setPageName] = useState(''); // 페이지명 상태
-
   useEffect(() => {
     const unsubscribe = nowPageStore.subscribe((state) => {
       setPageName(state.pageName);
     });
-
     return () => {
       unsubscribe();
     };
   }, []);
 
+  // 뒤로 가기 함수
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // -1을 전달하여 뒤로 이동
+  };
+
+
   return (
-    <div className={`d-flex align-items-center justify-content-center ${style.Header}`}>
-      <FontAwesomeIcon icon={faArrowLeft} />
+    <div className={style.Header}>
+      <FontAwesomeIcon icon={faArrowLeft} className={style.headerIcon} onClick={handleGoBack}/>
       <h1 className={style.pageName}>{pageName}</h1> 
-      <FontAwesomeIcon icon={faBell} />
+      <FontAwesomeIcon icon={faBell} className={style.headerIcon}/>
     </div>
   )
 }
