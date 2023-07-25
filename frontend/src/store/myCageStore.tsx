c8fa10184f41f7c4a3401b7360370ed046de8fa3
Koneweekk <rlagkswn37@naver.com>
2023-07-25 15:17:31 +0900

    S09P12A101-165 [Add, Design] 1. Design Cage List 2. Add page to Add Cage

--- frontend/src/store/myCageStore.tsx
+++ frontend/src/store/myCageStore.tsx
@@ -28,11 +28,11 @@ export const myCagesStore = create<myCages>() (
   persist(
     set => ({
       cages : [
-        {id : 1, cageId : 1, cageName: '1번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'cage1.png'},
-        {id : 1, cageId : 2, cageName: '2번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'cage2.png'},
-        {id : 1, cageId : 3, cageName: '3번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'cage1.png'},
-        {id : 1, cageId : 4, cageName: '4번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'cage2.png'},
-        {id : 1, cageId : 5, cageName: '5번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'cage1.png'},
+        {id : 1, cageId : 1, cageName: '1번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'Ball_Python.jpg'},
+        {id : 1, cageId : 2, cageName: '2번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'Black_Rat_Snake.jpg'},
+        {id : 1, cageId : 3, cageName: '3번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'Boa_Constrictor.jpg'},
+        {id : 1, cageId : 4, cageName: '4번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: "Bredl's_Python.jpg"},
+        {id : 1, cageId : 5, cageName: '5번', alarm : [new Date("2023-07-24")], setTemp : 30, setHum : 60, setUv : true, using : true, createdAt : new Date(), cageImg: 'Burmese_Python.jpg'},
       ],
       addCage : (cage:myCage) => console.log(cage),
       deleteCage : (id:Number) => console.log(id),
