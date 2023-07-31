65dd674b67ad79d4945f63deea725524c264ff79
손세이 <seyi607@gmail.com>
2023-07-31 17:08:02 +0900

    [Add] Jira : S09P12A101-229, S09P12A101-230 1. makes serial communicate code to RPI4 2. makes lock flag code

--- iot/ESP/code/mqtt.h
+++ iot/ESP/code/mqtt.h
@@ -3,6 +3,8 @@
 
 char* topic = "serialnumber/sensorval";
 char* get_topic = "serialnumber/setval";
+char* common_topic = "actset";
+char* status_topic = "actstatus";
 
 EspMQTTClient client(
   WIFINAME,
@@ -16,6 +18,7 @@ EspMQTTClient client(
 
 struct MQTT {
   Info val;
+  Info set_val;
   String data;
 
   MQTT() {
@@ -28,8 +31,9 @@ struct MQTT {
     client.enableOTA();
   }
 
-  //데이터값을 Json으로 변환
+  //data to JSON format
   void makeJson() {
+    data = "";
     DynamicJsonDocument doc(200);
 
     char res[20] = { 0, };
@@ -39,27 +43,69 @@ struct MQTT {
     doc["Humid"] = res;
     doc["uv"] = String(val.light);
 
-    Serial.print("Json data : ");
-    serializeJson(doc, Serial);
-    Serial.println();
+    // Serial.print("Json data : ");
+    // serializeJson(doc, Serial);
+    // Serial.println();
     serializeJson(doc, data);
   }
 
+  String makeStatusJson(Status status) {
+    DynamicJsonDocument doc(200);
+
+    char res[20] = { 0, };
+    sprintf(res, "%d", status.led);
+    doc["LED"] 
+    = res;
+    String tmp = "";
+    if(status.heat) tmp = "ON";
+    else tmp = "OFF";
+    doc["heat_pad"] = tmp;
+    if(status.cooling) tmp = "ON";
+    else tmp = "OFF";
+    doc["cooling_fan"] = tmp;
+    if(status.humidifier) tmp = "ON";
+    else tmp = "OFF";
+    doc["humidifier"] = tmp;
+    if(status.waterfall) tmp = "ON";
+    else tmp = "OFF";
+    doc["waterfall"] = tmp;
+
+    sprintf(res, "%.1f", val.temp);
+    doc["Temp"] = res;
+    sprintf(res, "%.1f", val.humid);
+    doc["Humid"] = res;
+    // Serial.print("Json data : ");
+    // serializeJson(doc, Serial);
+    // Serial.println();
+    serializeJson(doc, data);
+    return data;
+  }
+  
   void updataData(Info info) {
     val.temp = info.temp;
     val.humid = info.humid;
     val.light = info.light;
   }
 
+  void errorTx(char* err_topic, char* err_type) {
+    client.publish(err_topic, err_type);
+  }
   void tx() {
-    makeJson();
     client.publish(topic, data);
     data = "";
   }
-
-  bool errorCheck(StaticJsonDocument<200> doc) {
-    JsonVarient error_temp = doc["temp"];
-    JsonVarient error_humid = doc["humid"];
-    JsonVarient error_uv = doc["uv"];
+  void statustx() {
+    client.publish(status_topic, data);
+    data = "";
   }
+  // bool errorCheck(StaticJsonDocument<200> doc) {
+  //   JsonVarient error_temp = doc["temp"];
+  //   JsonVarient error_humid = doc["humid"];
+  //   JsonVarient error_uv = doc["uv"];
+
+  //   if(error_temp.isNull() || error_humid.isNull() || error_uv.isNull()) {
+  //     return 1;
+  //   }
+  //   return 0;
+  // }
 };
---
+++ iot/ESP/code/uartserial.h
@@ -0,0 +1,42 @@
+#include "header.h"
+
+HardwareSerial mySerial(2);
+
+struct UartSerial {
+  String data;
+
+  UartSerial() {
+    data = "";
+  }
+
+  void init() {
+    mySerial.begin(115200, SERIAL_8N1, 16, 17);
+  }
+
+  void statusTx(const char* send_data) {
+    mySerial.write(send_data);
+    // Serial.println(send_data);
+  }
+
+  Status statusRx() {
+    Status flag;
+    if (mySerial.available() > 0) {
+      data = mySerial.readString();
+      Serial.println(data);
+      StaticJsonDocument<200> doc;
+      deserializeJson(doc, data);
+
+      flag.led = doc["LED"];
+      flag.waterfall = doc["waterfall"];
+      flag.cooling = doc["cooling_fan"];
+      flag.heat = doc["heat_pad"];
+      flag.humidifier = doc["humidifier"];
+      flag.islock = doc["lock"];
+      
+      Serial.println();
+      return flag;
+    }
+    return {0,0,0,0,-1,1};
+  }
+};
+
