9939b8af1c61cb22fba690179588efdf801f17ad
injog <injog@naver.com>
2023-08-02 09:22:35 +0100

    [Add] Jira : S09P12A101-265, S09P12A101-267 1. change design on kiosk 2. add dark mode and left hand mode

--- iot/kiosk/main.py
+++ iot/kiosk/main.py
@@ -11,7 +11,8 @@ import json
 
 #multi threading
 import threading
-import time
+from time import sleep
+
 
 def is_on(i):
     if i == "ON":
@@ -19,12 +20,15 @@ def is_on(i):
     else:
         return False
 
+
 def is_off(i):
     if i == "OFF":
         return True
     else:
         return False
 
+
+# thread for recieving Actuator and sense data from ESP32 with Serial
 def recieve_thread():
     while True:
         if ser.readable():
@@ -33,37 +37,25 @@ def recieve_thread():
             print(data)
             win.temp_val.setText(data["Temp"][0:2])
             win.humid_val.setText(data["Humid"][0:2])
-            if is_off(data["cooling_fan"]):
-                win.fan_btn.setText("ON")
-            else:
-                win.fan_btn.setText("OFF")
+            
+            win.fan_btn.setChecked(data["cooling_fan"])
+            win.waterfall_btn.setChecked(data["waterfall"])
+            win.humidifier_btn.setChecked(data["humidifier"])
+            win.heat_btn.setChecked(data["heat_pad"])
 
-            if is_off(data["waterfall"]):
-                win.waterfall_btn.setText("ON")
-            else:
-                win.waterfall_btn.setText("OFF")
 
-            if is_off(data["humidifier"]):
-                win.humidifier_btn.setText("ON")
-            else:
-                win.humidifier_btn.setText("OFF")
-
-            if is_off(data["heat_pad"]):
-                win.heat_btn.setText("ON")
-            else:
-                win.heat_btn.setText("OFF")
-
+#transmit Actuator data to ESP32 with Serial
 def act_publish():
     pub_data = { "LED" : 0, "heat_pad" : False, "cooling_fan" : False, "humidifier" : False, "waterfall" : False, "lock" : 1}
-    if win.lock_btn.text() == "UNLOCK":
+    if not win.lock_btn.isChecked():
         return
     else:
         pub_data["lock"] =  0
     pub_data["LED"] = win.horizontalSlider.value()
-    pub_data["heat_pad"] = is_off(win.heat_btn.text())
-    pub_data["cooling_fan"] = is_off(win.fan_btn.text())
-    pub_data["humidifier"] = is_off(win.humidifier_btn.text())
-    pub_data["waterfall"] = is_off(win.waterfall_btn.text())
+    pub_data["heat_pad"] = win.heat_btn.isChecked()
+    pub_data["cooling_fan"] = win.fan_btn.isChecked()
+    pub_data["humidifier"] = win.humidifier_btn.isChecked()
+    pub_data["waterfall"] = win.waterfall_btn.isChecked()
     pub_data = json.dumps(pub_data)
     ser.write(pub_data.encode('utf-8'))
     
@@ -77,68 +69,79 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.showFullScreen()
         self.setCursor(Qt.BlankCursor)
         pass
-    
+   
+    #btn signal function for actuator
     def fan_on(self):
-        print("fan")
-        if win.lock_btn.text() == "UNLOCK":
+        if win.lock_btn.isChecked():
+            print("fan")
+            act_publish()
+            sleep(1)
             return
-        if is_on(self.fan_btn.text()):
-            self.fan_btn.setText("OFF")
-        else:
-            self.fan_btn.setText("ON")
-        act_publish()
-        time.sleep(0.5)
+        win.fan_btn.setChecked(not win.fan_btn.isChecked())
+        print("locked")
 
     def heat_on(self):
-        print("heat")
-        if win.lock_btn.text() == "UNLOCK":
+        if win.lock_btn.isChecked():
+            print("heat")
+            act_publish()
+            sleep(1)
             return
-        if is_on(self.heat_btn.text()):
-            self.heat_btn.setText("OFF")
-        else:
-            self.heat_btn.setText("ON")
-        act_publish()
-        time.sleep(0.5)
+        win.heat_btn.setChecked(not win.heat_btn.isChecked())
+        print("locked")
 
     def humidifier_on(self):
-        print("humi")
-        if win.lock_btn.text() == "UNLOCK":
+        if win.lock_btn.isChecked():
+            print("humi")
+            act_publish() 
+            sleep(1)
             return
-        if is_on(self.humidifier_btn.text()):
-            self.humidifier_btn.setText("OFF")
-        else:
-            self.humidifier_btn.setText("ON")
-        act_publish() 
-        time.sleep(0.5)
+        win.humidifier_btn.setChecked(not win.humidifier_btn.isChecked())
+        print("locked")
 
     def waterfall_on(self):
-        print("water")
-        if win.lock_btn.text() == "UNLOCK":
+        if win.lock_btn.isChecked():
+            print("waterfall")
+            act_publish()
+            sleep(1)
             return
-        if is_on(self.waterfall_btn.text()):
-            self.waterfall_btn.setText("OFF")
-        else:
-            self.waterfall_btn.setText("ON")
-        act_publish()
+        win.waterfall_btn.setChecked(not win.waterfall_btn.isChecked())
+        print("locked")
 
     def led_on(self):
-        if win.lock_btn.text() == "UNLOCK":
+        if win.lock_btn.isChecked():
+            print("led")
+            act_publish()
+            sleep(1)
             return
-        act_publish()
-    
+        print("locked")
+         
     def lock_on(self):
-        if self.lock_btn.text() == "LOCK":
-            self.lock_btn.setText("UNLOCK")
-        else:
-            self.lock_btn.setText("LOCK")
         act_publish()
-    
+        sleep(1)
+
+    # user friendly feature
+    def dark_mode_on(self):
+        if win.dark_mode_btn.isChecked():
+            win.setStyleSheet("background-color:#292A2D;")
+        else:
+            win.setStyleSheet("background-color:#F4F6F8;")
+
+    def left_mode_on(self):
+        if win.left_mode_btn.isChecked():
+            win.sense_layout.move(570,210)
+            win.actuator_layout.move(60,168)
+        else:
+            win.sense_layout.move(60,210)
+            win.actuator_layout.move(550,168)
+
 
 app = QApplication()
 win = MyApp()
 ser = serial.Serial('/dev/ttyAMA2', 115200)
 
 def main():
+    
+    # multithreading
     try:
         t1 = threading.Thread(target=recieve_thread)
         t1.start()
