5533d7c80f657195d72c6de568d2ebff398d25f5
YOUNPRIZE <ysang10@gmail.com>
2023-08-06 22:46:13 +0900

    [Add] temporary branch

---
+++ backend/src/main/java/com/ssafy/a101/api/response/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package com.ssafy.a101.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
