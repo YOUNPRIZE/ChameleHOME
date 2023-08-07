add005457e446c84d3af4321fb47555dc8d9bef8
ysang10 <ysang10@naver.com>
2023-08-07 13:25:33 +0900

    [Update] Update backend repo

--- backend/src/main/java/com/ssafy/a101/api/service/UserDetailService.java
+++ backend/src/main/java/com/ssafy/a101/api/service/UserDetailService.java
@@ -1,20 +1,20 @@
-//package com.ssafy.a101.api.service;
-//
-//import com.ssafy.a101.db.entity.User;
-//import com.ssafy.a101.db.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.stereotype.Service;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class UserDetailService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public User loadUserByUsername(String user_id) {
-//        return userRepository.findByUserId(user_id)
-//                .orElseThrow(() -> new IllegalArgumentException((user_id)));
-//    }
-//}
+package com.ssafy.a101.api.service;
+
+import com.ssafy.a101.db.entity.User;
+import com.ssafy.a101.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String user_id) {
+        return userRepository.findByUserId(user_id)
+                .orElseThrow(() -> new IllegalArgumentException((user_id)));
+    }
+}
