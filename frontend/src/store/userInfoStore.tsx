2ee8ebace2a02c1be3869e6548199d4e7bf853dc
Koneweekk <rlagkswn37@naver.com>
2023-08-07 15:39:55 +0900

    [Fix] 1.fix mqtt connect method from mqtt to wss

--- frontend/src/store/userInfoStore.tsx
+++ frontend/src/store/userInfoStore.tsx
@@ -1,6 +1,5 @@
 import {create} from 'zustand'
-import {persist} from 'zustand/middleware'
-import { useAxiosAuth } from 'hooks/useAxios';
+import { axiosAuth } from 'constants/AxiosFunc';
 
 // 현재 로그인 유저
 
@@ -8,59 +7,65 @@ import { useAxiosAuth } from 'hooks/useAxios';
 export interface User {
   id: number;
   userId: string | undefined;
-  userPw: string | undefined,
+  password: string | undefined,
   nickName: string;
   phoneNumber: string;
   isLoggedIn:boolean;
 }
 
-
+// 상태 정보 정의
 export interface userInfoState {
   user: User,
-  login: (inputId: string|undefined, inputPW: string|undefined) => void;
+  login: (id:string|undefined, pw:string|undefined) => void;
   logout: () => void;
 }
 
 // 로그인 유저 상태 정보
 export const userInfoStore = create<userInfoState>()(
-  persist(
     set => ({
+      // 기본 유저 정보
       user: { 
         id: 0,
         userId: "",
-        userPw: "",
+        password: "",
         nickName: "",
         phoneNumber: "",
         isLoggedIn: false
       },
       // 로그인 메서드
-      login: (inputId:string|undefined, inputPW: string|undefined) =>
+      login: async (id, pw) =>
         { 
-          return set((state) => ({
-            ...state,
-            user: {
-              id: 1,
-              userId: inputId,
-              userPw: inputPW,
-              nickName: 'Test',
-              phoneNumber: '010-0000-0000',
-              isLoggedIn: true
+          // 로그인 시도
+          try {
+            // 로그인 함수 구현 (연결 후 수정해야함)
+            const userDatas = await axiosAuth("user", "GET");
+            const user = userDatas.find((userData:User | undefined) => 
+            userData?.userId === id && userData?.password === pw);
+            // id와 pw가 적절할 경우
+            if (user) {
+              return set((state) => ({
+                ...state,
+                user: {...user, isLoggedIn: true}
+              }))
+            // 해당 유저가 없을 경우
             }
-          }))
+          // api 에러 
+          } catch (error) {
+            console.log(error); // 에러를 설정합니다.
+          }
         },
       // 로그아웃 메서드
       logout: () => 
         { 
           return set((state) => ({
             ...state,
-            id:0,
+            user: {id:0,
             userId:"",
+            password: "",
             nickName:"",
             phoneNumber:"",
-            isLoggedIn: false
+            isLoggedIn: false}
           }))
         }
-    }),
-    {name: 'userInfo'}
-  )
+    })
 )
