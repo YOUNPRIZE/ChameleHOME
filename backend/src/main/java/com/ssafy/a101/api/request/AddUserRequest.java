9940f5dfefebf2982173c047aaf9d7bc552419e2
ysang10 <ysang10@naver.com>
2023-08-08 15:06:30 +0900

    [Update] Update backend files

--- backend/src/main/java/com/ssafy/a101/api/request/AddUserRequest.java
+++ backend/src/main/java/com/ssafy/a101/api/request/AddUserRequest.java
@@ -1,15 +1,34 @@
 package com.ssafy.a101.api.request;
 
+import com.ssafy.a101.db.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 public class AddUserRequest {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private String userId;
     private String password;
     private String nickname;
     private Long number;
     private String user_img;
+
+    public User toEntity() {
+        return User.builder()
+                .userId(getUserId())
+                .password(bCryptPasswordEncoder.encode(getPassword()))
+                .nickname(getNickname())
+                .number(getNumber())
+                .user_img(getUser_img())
+                .build();
+    }
 }
