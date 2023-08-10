7465884347301f90179697d3ab6e54fc8f126f3d
mintae <alsxo824@naver.com>
2023-08-10 15:51:51 +0900

    [Tetst]

--- backend/src/main/java/com/ssafy/a101/api/controller/TokenController.java
+++ backend/src/main/java/com/ssafy/a101/api/controller/TokenController.java
@@ -6,12 +6,14 @@ import com.ssafy.a101.api.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin("*")
 public class TokenController {
 
     private final TokenService tokenService;
