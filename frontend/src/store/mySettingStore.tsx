f118b70bc6c290eb614b0978d60397f7ce16222a
Koneweekk <rlagkswn37@naver.com>
2023-08-10 12:53:41 +0900

    [Feat] S09P12A101-323 1. Add Animal Feature using test api

--- frontend/src/store/mySettingStore.tsx
+++ frontend/src/store/mySettingStore.tsx
@@ -1,63 +1,5 @@
-import { Today } from '@mui/icons-material';
 import {create} from 'zustand'
 import {persist} from 'zustand/middleware'
-
-const test1:{ [key: number]: Array<autoSetting> } = {
-  1: [
-    {
-      setting_pk: 1,
-      cage_id: 1,
-      time: "12:30",
-      set_temp: 30,
-      set_hum : null,
-      set_uv : null,
-    },
-    {
-      setting_pk: 2,
-      cage_id: 1,
-      time: "17:30",
-      set_temp: 25,
-      set_hum : null,
-      set_uv : null,
-    },
-    {
-      setting_pk: 3,
-      cage_id: 1,
-      time: "20:30",
-      set_temp: null,
-      set_hum : 50,
-      set_uv : null,
-    },
-    {
-      setting_pk: 4,
-      cage_id: 1,
-      time: "11:30",
-      set_temp: null,
-      set_hum : 50,
-      set_uv : null,
-    },
-    {
-      setting_pk: 5,
-      cage_id: 1,
-      time: "05:30",
-      set_temp: null,
-      set_hum : 50,
-      set_uv : null,
-    },
-    {
-      setting_pk: 6,
-      cage_id: 1,
-      time: "11:30",
-      set_temp: null,
-      set_hum : null,
-      set_uv : true,
-    },
-  ],
-  2 :[],
-  3 :[],
-  4 :[],
-  5 :[],
-}
 
 const test2:{ [key: number]: Array<alarmSetting> } = {
   1: [
@@ -87,7 +29,7 @@ export interface autoSetting {
 }
 
 interface autoSettingState {
-  settingsInCages: { [key: number]: Array<autoSetting> };
+  settings: Array<autoSetting>;
   addSetting: (setting: autoSetting) => void;
   updateSetting: (setting: autoSetting) => void;
   deleteSetting: (id: number) => void;
@@ -97,11 +39,19 @@ interface autoSettingState {
 export const autoSettingStore = create<autoSettingState>()(
   persist(
   set => ({
-      settingsInCages: test1,
-      addSetting: (setting: autoSetting) => {console.log(setting)},
+      settings: [],
+      // 세팅 추가
+      addSetting: (setting: autoSetting) => {
+        set((state) => {
+          state.settings.push(setting);
+          return {...state}
+        })
+      },
       updateSetting: (setting: autoSetting) => {console.log(setting)},
       deleteSetting: (id: number) => {console.log(id)},
-      setSetting: (settings: Array<autoSetting>) => {console.log(settings)},
+      // 기존 세팅 데이터 저장
+      setSetting: (settings: Array<autoSetting>) => {
+        set(state => {return {...state, settings:settings}})},
     }),
   {name:'autoSettings'}
   )
