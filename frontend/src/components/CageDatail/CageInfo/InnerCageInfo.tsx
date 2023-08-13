4994919e3d752d12bb83483093d60e189e68d395
Hanjuki <hanju1997@gmail.com>
2023-08-13 15:56:44 +0900

    [Fix] Fixing mqtt connect

--- frontend/src/components/CageDatail/CageInfo/InnerCageInfo.tsx
+++ frontend/src/components/CageDatail/CageInfo/InnerCageInfo.tsx
@@ -37,13 +37,13 @@ export default function InnerCageInfo(props:{myCage:myCage|undefined}):JSX.Eleme
     }
 
     // Mqtt 연결
-    const client = new Client("i9a101.p.ssafy.io", 9001, "client");
+    const client = new Client("0.0.0.0", 9001, "client");
     clientRef.current = client;
     if (!client.isConnected()) {
       client.connect({
         userName: "FRONT",
         password: "1234",
-        useSSL:true,
+        // useSSL:true,
         // mqttVersion:4,
         // 커넥트에 성공(구독)
         onSuccess: () => { 
