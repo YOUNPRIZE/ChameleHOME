bbffbe9b79d1d75573e8e6978b861e3b4b5db4be
Koneweekk <rlagkswn37@naver.com>
2023-08-14 17:47:47 +0900

    [Add] 1. Add loading window

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,15 +1,14 @@
 // 훅 import 
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react';
+import { Client } from 'paho-mqtt';
 // 상태 정보 import
 import { nowPageStore } from 'store/myPageStore';
+import { mqttClientStore } from 'store/mqttClientStore';
 // 컴포넌트 import
 import CageBox from 'components/Main/CageBox';
 import DictionaryBox from 'components/Main/DicionaryBox';
 import ItemBox from 'components/Main/ItemBox';
 // 스타일 import
-import style from 'styles/Main.module.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 export default function Main():JSX.Element {
   // 페이지명 변경
@@ -17,6 +16,16 @@ export default function Main():JSX.Element {
   useEffect(() => {
     changePage("홈");
   }, [])
+
+  // mqtt 브로커 커넥트 시도 완료까지 로딩
+  const [endConnect, setEndConnect] = useState(false);
+  const mqttclient = mqttClientStore(state => state.client);
+  const setClient = mqttClientStore(state => state.setClient);
+  console.log(mqttclient)
+  useEffect(() => {
+  }, [])  
+
+
 
   // 페이지 렌더링
   return (
