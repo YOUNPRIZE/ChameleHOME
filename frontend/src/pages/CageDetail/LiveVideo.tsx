0b00ad77666af6569aaf33eff95f317699914249
Koneweekk <rlagkswn37@naver.com>
2023-08-14 14:01:43 +0900

    [Feat] S09P12A101-366 1. Add store Feature 2. connect backend with store feature

--- frontend/src/pages/CageDetail/LiveVideo.tsx
+++ frontend/src/pages/CageDetail/LiveVideo.tsx
@@ -1,7 +1,6 @@
 // 훅|함수 import 
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react'
-import html2canvas from 'html2canvas';
 // 상태 정보 import
 import { nowPageStore } from 'store/myPageStore';
 // 컴포넌트 import
@@ -19,61 +18,14 @@ export default function LiveVideo():JSX.Element {
     changePage("실시간 영상");
   }, [changePage])
 
-  // 영상 캡쳐 함수
-  // const handleCapture = async() => {
-  //   // const target = document.getElementById('content');
-  //   // if (!target) {
-  //   //   return alert('결과 저장에 실패했습니다.');
-  //   // }
-  //   // console.log(target)
-  //   html2canvas(document.body).then((canvas) => {
-  //     const link = document.createElement('a');
-  //     document.body.appendChild(link);
-  //     link.href = canvas.toDataURL('image/png');
-  //     link.download = 'result.png';
-  //     link.click();
-  //     document.body.removeChild(link);
-  //   });
-  // }
 
-  const handleCapture = async (): Promise<void> => {
-    const iframe = document.getElementById('content') as HTMLIFrameElement;
-    
-    if (!iframe) {
-      return alert('결과 저장에 실패했습니다.');
-    }
-
-    try {
-      console.log(iframe)
-      const iframeDoc = iframe.contentDocument;
-      if (!iframeDoc) {
-        throw new Error('Failed to access iframe content document');
-      }
-
-      const iframeBody = iframeDoc.body;
-      if (!iframeBody) {
-        throw new Error('Failed to access iframe body');
-      }
-
-      const canvas = await html2canvas(iframe);
-
-      const link = document.createElement('a');
-      document.body.appendChild(link);
-      link.href = canvas.toDataURL('image/png');
-      link.download = 'result.png';
-      link.click();
-      document.body.removeChild(link);
-    } catch (error) {
-      console.error('Error during capture:', error);
-    }
-  }
 
   return (
     <>
       {/* 동영상 컨테이너 */}
       <VideoBox/>
       {/* 카메라 무빙 버튼 */}
-      <MoveBtnBox handleCapture={handleCapture}/>
+      <MoveBtnBox/>
     </>
   )
 }
