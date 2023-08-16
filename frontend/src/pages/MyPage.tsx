cfeb90c72a767909ef965b23630d00af78b35fdc
Koneweekk <rlagkswn37@naver.com>
2023-08-17 01:35:04 +0900

    [Fix] 1. Handling Exceptions Login and Signup

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -3,7 +3,9 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 // 상태정보 import
 import { nowPageStore } from 'store/myExtraStore';
-import { userInfoStore, userInfoState } from 'store/userInfoStore';
+import { userInfoStore } from 'store/userInfoStore';
+// 컴포넌트 import
+import UserInfoBox from 'components/MyPage/UserInfoBox';
 // 스타일 import
 import style from 'styles/MyPage.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -19,14 +21,6 @@ export default function MyPage():JSX.Element {
     changePage("마이페이지");
   }, [])
 
-  //로그아웃 함수
-  const navigate = useNavigate();
-  const userInfo:userInfoState = userInfoStore();
-  const handleLogout = ():void => {
-    userInfo.deleteUserInfo();
-    navigate('/Login')
-  }
-
   // 페이지 렌더링
   return(
     <>
@@ -34,24 +28,9 @@ export default function MyPage():JSX.Element {
       <div className={style.imgContainer}>
         <img src={testImg} alt="" className={style.profileImg}/>
       </div>
-      {/* 카드(사용자 정보 영역) */}
       <div className={style.cardContainer}>
-        <div className={style.myInfo}>
-          <div className={style.farContainer}>
-            <h2 className={style.myName}>{userInfo.user.userId}</h2>
-            <a onClick={handleLogout} className={style.logOut}>
-              <FontAwesomeIcon icon={faArrowRightToBracket} rotation={180}/>로그아웃
-            </a>
-          </div>
-          <div className={style.infoContainer}>
-            <FontAwesomeIcon icon={faSpellCheck} className={style.infoIcon}/>
-            <span className={style.infoText}>{userInfo.user.nickname}</span>
-          </div>
-          <div className={style.infoContainer}>
-            <FontAwesomeIcon icon={faEnvelope} className={style.infoIcon}/>
-            <span className={style.infoText}>{userInfo.user.email}</span>
-          </div>
-        </div>
+        {/* 사용자 정보 영역 */}
+        <UserInfoBox/>
         {/* 회원 정보 수정, 탈퇴 영역 */}
         <div className={`${style.farContainer} ${style.btnContainer}`}>
           <button className={style.btn}>회원정보수정</button>
