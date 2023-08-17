423d85aac8aedb5ccba7036416bea947c7582eeb
Koneweekk <rlagkswn37@naver.com>
2023-08-17 11:23:40 +0900

    [Feat, Fix] 1. Add Update and Delete Userinfo 2. Cancel adding cage if snum is alreay registered

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,16 +1,18 @@
 // 훅 import
-import { useEffect } from 'react';
+import { useEffect, useState  } from 'react';
 import { useNavigate } from 'react-router-dom';
 // 상태정보 import
 import { nowPageStore } from 'store/myExtraStore';
 import { userInfoStore } from 'store/userInfoStore';
 // 컴포넌트 import
+import AuthModal from 'components/MyPage/AuthModal';
 import UserInfoBox from 'components/MyPage/UserInfoBox';
+import AuthBtnBox from 'components/MyPage/AuthBtnBox';
 // 스타일 import
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import style from 'styles/MyPage.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightToBracket, faSpellCheck, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import testImg from 'assets/test.jpg'
 
 
@@ -21,6 +23,20 @@ export default function MyPage():JSX.Element {
     changePage("마이페이지");
   }, [])
 
+  // 변수 선언
+  const [show, setShow] = useState(false);
+  const [authAction, setAuthAction] = useState(0)
+
+  // 모달창 끄기 함수
+  const handleCloseModal = () => {
+    setShow(false)}
+  ;
+  // 모달창 켜기 함수
+  const handleShowModal = (action:number) => {
+    setAuthAction(action)
+    setShow(true)
+  };
+    
   // 페이지 렌더링
   return(
     <>
@@ -32,11 +48,10 @@ export default function MyPage():JSX.Element {
         {/* 사용자 정보 영역 */}
         <UserInfoBox/>
         {/* 회원 정보 수정, 탈퇴 영역 */}
-        <div className={`${style.farContainer} ${style.btnContainer}`}>
-          <button className={style.btn}>회원정보수정</button>
-          <button className={style.btn}>회원탈퇴</button>
-        </div>
+        <AuthBtnBox handleShowModal={handleShowModal}/>
       </div>
+      {/* 회원 정보 수정 or 삭제 모달 */}
+      <AuthModal show={show} authAction={authAction} close={handleCloseModal}/>
     </>
   )
 }
